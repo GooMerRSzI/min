0e38afe672a6426f755f35d54990709225a295ea
GooMerRSzI <gettfgowdv@rambler.ru>
2024-06-22 04:11:44 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const showImageBtn = document.getElementById('showImageBtn');
 const imageContainer = document.getElementById('imageContainer');
 const placeholderContainer = document.getElementById('placeholderContainer');
-
+const randomImage = document.getElementById('randomImage');
 let currentImageElement; // Reference to the current image element
 
 // Replace with your actual image URLs
@@ -14,24 +14,23 @@ const images = [
 ];
 showImageBtn.addEventListener('click', () => {
     const randomIndex = Math.floor(Math.random() * images.length);
-    const newImageURL = images[randomIndex];
+    randomImage.src = images[randomIndex];
 
-    if (currentImageElement) { // Check if there's a current image
-        currentImageElement.src = newImageURL; // Replace the source of the current image
-    } else { // If no current image, create a new one
-        const imageElement = document.createElement('img');
-        imageElement.src = newImageURL;
-        imageElement.id = 'randomImage';
-        imageElement.classList.add('image-container-hidden');
+//    imageElement.id = 'randomImage';
+//    imageElement.classList.add('image-container-hidden');
+//
+//
+//    showImageBtn.classList.add('image-container-hidden');
+//    placeholderContainer.style.display = 'none'; // Hide placeholder after click
+//
+//    setTimeout(() => {
+//        imageElement.classList.remove('image-container-hidden');
+//        showImageBtn.classList.remove('image-container-hidden');
+//        currentImageElement = imageElement; // Set currentImageElement reference
+//    }, 1000);
+//    if (currentImageElement) { // Check if there's a current image
+//        currentImageElement.src = newImageURL; // Replace the source of the current image
+//    } else { // If no current image, create a new one
 
-        imageContainer.appendChild(imageElement);
-        showImageBtn.classList.add('image-container-hidden');
-        placeholderContainer.style.display = 'none'; // Hide placeholder after click
-
-        setTimeout(() => {
-            imageElement.classList.remove('image-container-hidden');
-            showImageBtn.classList.remove('image-container-hidden');
-            currentImageElement = imageElement; // Set currentImageElement reference
-        }, 1000);
-    }
+//    }
 });
